51e826e5654df3b2942d402118f35b6515bbf847
WZY <wzt@qq.com>
2022-04-21 16:27:50 +0800

    hotfix commit2

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("version - 1");
         System.out.println("hotfix commit1");
+        System.out.println("hotfix commit2");
     }
 }
